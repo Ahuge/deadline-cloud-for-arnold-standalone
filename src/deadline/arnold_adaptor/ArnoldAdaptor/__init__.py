8bc24b8dff88552d5c72d0068cea45dee42bb98c
Alex Hughes <ahughesalex@gmail.com>
2024-10-24 09:15:42 -0700

    Arnold Adaptor

---
+++ src/deadline/arnold_adaptor/ArnoldAdaptor/__init__.py
@@ -0,0 +1,9 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+from .__main__ import main
+from .adaptor import ArnoldAdaptor
+
+__all__ = [
+    "ArnoldAdaptor",
+    "main",
+]
